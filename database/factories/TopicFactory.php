c6e32d1837e1be359484e5a6b5b64b2b4670909e
zhongcheng <1332582879@qq.com>
2023-07-13 15:59:26 +0800

    话题排序

--- database/factories/TopicFactory.php
+++ database/factories/TopicFactory.php
@@ -12,12 +12,18 @@ class TopicFactory extends Factory
     public function definition()
     {
         $sentence = $this->faker->sentence();
+        // 随机取一个月以内的时间
+        $updated_at = $this->faker->dateTimeThisMonth();
 
+        // 传参为生成最大时间不超过，因为创建时间需永远比更改时间要早
+        $created_at = $this->faker->dateTimeThisMonth($updated_at);
         return [
             // $this->faker->name,
             'title' => $sentence,
             'body' => $this->faker->text(),
             'excerpt' => $sentence,
+            'created_at' => $created_at,
+            'updated_at' => $updated_at,
             'user_id' => $this->faker->randomElement([1,2,3,4,5,6,7,8,9,10]),
             'category_id' => $this->faker->randomElement([1,2,3,4]),
         ];
